c20480953d7d6f2246f5a24a955d67bfbe86d282
tin <tin.pt@ntnc.vn>
2023-06-04 17:05:26 +0700

    Initial Commit

---
+++ I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Administrator on 04/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
